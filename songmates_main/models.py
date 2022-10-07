9f7dbb82cbddd78a418597dc7174eae4f0ca573b
Andy G <andyguttridge123@gmail.com>
2022-10-07 08:35:56 +0000

    Feature: add fields to profile model

--- songmates_main/models.py
+++ songmates_main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 from .genres import Genres
 
 
@@ -7,9 +8,22 @@ class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE,
                              related_name="profile")
     slug = models.SlugField(max_length=200, unique=True)
-    friends = models.ManyToManyField("self", null=True, blank=True,
+    friends = models.ManyToManyField("self", blank=True,
                                      )
+    image = CloudinaryField('image', default='placeholder')
     biog = models.TextField(max_length=500, null=True, blank=True)
     genre1 = models.CharField(choices=Genres.choices, max_length=3, null=True,
                               blank=True)
+    genre2 = models.CharField(choices=Genres.choices, max_length=3, null=True,
+                              blank=True)
+    genre3 = models.CharField(choices=Genres.choices, max_length=3, null=True,
+                              blank=True)
+    genre4 = models.CharField(choices=Genres.choices, max_length=3, null=True,
+                              blank=True)
+    genre5 = models.CharField(choices=Genres.choices, max_length=3, null=True,
+                              blank=True)
     instru_skill1 = models.CharField(max_length=30, null=True, blank=True)
+    instru_skill2 = models.CharField(max_length=30, null=True, blank=True)
+    instru_skill3 = models.CharField(max_length=30, null=True, blank=True)
+    instru_skill4 = models.CharField(max_length=30, null=True, blank=True)
+    instru_skill5 = models.CharField(max_length=30, null=True, blank=True)
