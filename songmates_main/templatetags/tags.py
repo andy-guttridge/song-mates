92fd7cf5fb74b3af2e9a7d3d4bff2b2d861926b9
Andy G <andyguttridge123@gmail.com>
2022-10-31 17:54:24 +0000

    Feature: add count of incoming messages to navbar icon

--- songmates_main/templatetags/tags.py
+++ songmates_main/templatetags/tags.py
@@ -1,6 +1,6 @@
 from django import template
 from django.db.models import Q
-from songmates_main.models import CollabRequest
+from songmates_main.models import CollabRequest, Message
 
 register = template.Library()
 
@@ -19,3 +19,14 @@ def number_of_collab_requests(request):
             Q(from_user=request.user) | Q(to_user=request.user)
             ).count()
     return num_collab_requests
+
+
+@register.simple_tag
+def number_of_incoming_messages(request):
+    """
+    Find out how many incoming messages there are for the current user and
+    make available as a template tag.
+    """
+    num_incoming_messages = Message.objects.filter(
+            to_user=request.user).filter(to_deleted=False).count()
+    return num_incoming_messages
