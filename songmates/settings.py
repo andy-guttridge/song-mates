784805534d564d025d858bb866cbcb4f5d5151d4
Andy G <andyguttridge123@gmail.com>
2022-11-10 10:53:11 +0000

    Deployment: set debug to False for testing

--- songmates/settings.py
+++ songmates/settings.py
@@ -39,7 +39,7 @@ LOGOUT_REDIRECT_URL = '/'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ALLOWED_HOSTS = ['songmates.herokuapp.com', 'localhost']
 
